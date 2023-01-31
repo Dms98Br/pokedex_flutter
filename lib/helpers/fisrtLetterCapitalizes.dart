203e9fbf1005d8a17d87c5fd8c4394042d395976
Daniel Moya da Silva <danimoyasilva.dev@gmail.com>
2023-01-31 11:17:21 -0300

    fix: altered the replaceAll for replaceFirst

--- lib/helpers/fisrtLetterCapitalizes.dart
+++ lib/helpers/fisrtLetterCapitalizes.dart
@@ -2,7 +2,7 @@ fisrtLetterCapitalizes(String text) {
   var words = text.toLowerCase();
   for (var i = 0; i < words.length; i++) {
     var letterUpperCase = words[0].toUpperCase();
-    words = words.replaceAll(words[0], letterUpperCase);
+    words = words.replaceFirst(words[0], letterUpperCase);
   }
   return words;
 }
