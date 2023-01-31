d6aae7ad06c8176dcb5951a389a3b1186b9afdde
Daniel Moya da Silva <danimoyasilva.dev@gmail.com>
2023-01-31 02:33:23 -0300

    create: component of the appbar

---
+++ lib/components/appBar.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:pokedex/themes/theme_color.dart';
+
+class AppBarComponent extends StatelessWidget {
+  const AppBarComponent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      iconTheme: const IconThemeData(
+        color: Colors.black,
+      ),
+      flexibleSpace: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+          begin: Alignment.centerLeft,
+          end: Alignment.centerRight,
+          colors: <Color>[
+            ThemeColors.appBarColor1,
+            ThemeColors.backgroudColor2,
+          ],
+        )),
+      ),
+    );
+  }
+}
