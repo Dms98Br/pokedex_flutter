9f3d90d12ed00a4cf5dc3e1465fb76e37bf6ea1a
Daniel Moya da Silva <danimoyasilva.dev@gmail.com>
2023-02-01 12:22:32 -0300

    create: generate list and color of pokemons

---
+++ lib/services/pokemon_service.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:pokedex/helpers/generateColors.dart';
+import 'package:pokedex/data/pokemonsList.dart';
+import 'package:pokedex/screens/models/pokemons.dart';
+
+class PokemonService {
+  Future<List<Pokemons>> getAll() async {
+    List<Pokemons> list = [];
+    for (var pokemon in pokemonsList) {
+      final color = await generateColor.getColorImage(
+          image: AssetImage(pokemon['image']));
+      pokemon['color'].add(color);
+
+      list.add(Pokemons.fromMap(pokemon));
+    }
+    return list;
+  }
+}
