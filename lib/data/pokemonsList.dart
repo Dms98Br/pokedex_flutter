0c5f3e3d56dc8fbc6602771b5826ecbd9c286bd9
Daniel Moya da Silva <danimoyasilva.dev@gmail.com>
2023-02-01 12:17:57 -0300

    feat: new pokemons and new formated json

--- lib/data/pokemonsList.dart
+++ lib/data/pokemonsList.dart
@@ -1,58 +1,159 @@
+import 'package:flutter/material.dart';
+
 List pokemonsList = [
+  {
+    "id": "86",
+    "name": "seel",
+    "type": ["water"],
+    "image": "assets/images/pokemons/86_seel_86.png",
+    "color": [],
+    "abilities": [],
+    "evolutions": [
+      {
+        "id": "87",
+        "name": "dewgong",
+        "type": ['water', 'ice'],
+        "image": "assets/images/pokemons/87_dewgong_87.png"
+      },
+    ],
+  },
+  {
+    "id": "87",
+    "name": "dewgong",
+    "type": ['water', 'ice'],
+    "image": "assets/images/pokemons/87_dewgong_87.png",
+    "color": [],
+    "abilities": [],
+    "evolutions": [],
+  },
   {
     "id": "133",
     "name": "eevee",
-    "type": "normal",
+    "type": ["normal"],
     "image": "assets/images/pokemons/133_eevee_133.png",
-    "evolution": [
+    "color": [],
+    "abilities": [],
+    "evolutions": [
       {
         "id": "134",
         "name": "vaporeon",
-        "type": "water",
+        "type": ["water"],
         "image": "assets/images/pokemons/134_vaporeon_134.png"
       },
       {
         "id": "135",
         "name": "jolteon",
-        "type": "electric",
+        "type": ["electric"],
         "image": "assets/images/pokemons/135_jolteon_135.png"
       },
       {
         "id": "136",
         "name": "flareon",
-        "type": "fire",
+        "type": ["fire"],
         "image": "assets/images/pokemons/136_flareon_136.png"
       },
       {
         "id": "196",
         "name": "espeon",
-        "type": "psychic",
+        "type": ["psychic"],
         "image": "assets/images/pokemons/196_espeon_196.png"
       },
       {
         "id": "197",
         "name": "umbreon",
-        "type": "dark",
+        "type": ["dark"],
         "image": "assets/images/pokemons/197_umbreon_197.png"
       },
       {
         "id": "470",
         "name": "leafeon",
-        "type": "grass",
+        "type": ["grass"],
         "image": "assets/images/pokemons/470_leafeon_470.png"
       },
       {
         "id": "471",
         "name": "glaceon",
-        "type": "ice",
+        "type": ["ice"],
         "image": "assets/images/pokemons/471_glaceon_471.png"
       },
       {
         "id": "700",
         "name": "sylveon",
-        "type": "fairy",
+        "type": ["fairy"],
         "image": "assets/images/pokemons/700_sylveon_700.png"
       },
     ]
+  },
+  {
+    "id": "134",
+    "name": "vapoeren",
+    "type": ["water"],
+    "image": "assets/images/pokemons/134_vaporeon_134.png",
+    "color": [],
+    "abilities": [],
+    "evolutions": [],
+  },
+  {
+    "id": "135",
+    "name": "jolteon",
+    "type": ["electric"],
+    "image": "assets/images/pokemons/135_jolteon_135.png",
+    "color": [],
+    "abilities": [],
+    "evolutions": [],
+  },
+  {
+    "id": "136",
+    "name": "flareon",
+    "type": ["fire"],
+    "image": "assets/images/pokemons/136_flareon_136.png",
+    "color": [],
+    "abilities": [],
+    "evolutions": [],
+  },
+  {
+    "id": "196",
+    "name": "espeon",
+    "type": ["psychic"],
+    "image": "assets/images/pokemons/196_espeon_196.png",
+    "color": [],
+    "abilities": [],
+    "evolutions": [],
+  },
+  {
+    "id": "197",
+    "name": "umbreon",
+    "type": ["dark"],
+    "image": "assets/images/pokemons/197_umbreon_197.png",
+    "color": [],
+    "abilities": [],
+    "evolutions": [],
+  },
+  {
+    "id": "470",
+    "name": "leafeon",
+    "type": ["grass"],
+    "image": "assets/images/pokemons/470_leafeon_470.png",
+    "color": [],
+    "abilities": [],
+    "evolutions": [],
+  },
+  {
+    "id": "471",
+    "name": "glaceon",
+    "type": ["ice"],
+    "image": "assets/images/pokemons/471_glaceon_471.png",
+    "color": [],
+    "abilities": [],
+    "evolutions": [],
+  },
+  {
+    "id": "700",
+    "name": "sylveon",
+    "type": ["fairy"],
+    "image": "assets/images/pokemons/700_sylveon_700.png",
+    "color": [],
+    "abilities": [],
+    "evolutions": [],
   },
 ];
