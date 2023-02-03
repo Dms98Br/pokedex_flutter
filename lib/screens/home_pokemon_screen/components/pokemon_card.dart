9e59294068930cfea35dd62c3e0bbc12ff395474
Daniel Moya da Silva <danimoyasilva.dev@gmail.com>
2023-02-03 14:33:28 -0300

    update: creating component card pokemon

---
+++ lib/screens/home_pokemon_screen/components/pokemon_card.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:pokedex/screens/models/pokemons.dart';
+
+import '../../../components/pokemon_type.dart';
+import '../../../helpers/fisrtLetterCapitalizes.dart';
+import '../../../helpers/generateColors.dart';
+
+class PokemonCards extends StatelessWidget {
+  final Pokemons pokemon;
+  const PokemonCards({super.key, required this.pokemon});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 6, bottom: 6, right: 12, left: 12),
+      child: InkWell(
+        onTap: () {
+          Navigator.pushNamed(
+            context,
+            "details",
+            arguments: pokemon,
+          );
+        },
+        child: Container(
+          height: 100,
+          width: 100,
+          decoration: BoxDecoration(
+            color: pokemon.color.isNotEmpty
+                ? pokemon.color[0]
+                : const Color(0xFF404040),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 20, top: 5),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "#${pokemon.id}",
+                      style: TextStyle(
+                        color:
+                            generateColor().colorLightOrDark(pokemon.color[0])
+                                ? Colors.black54
+                                : Colors.white54,
+                      ),
+                    ),
+                    Text(
+                      fisrtLetterCapitalizes(pokemon.name),
+                      style: TextStyle(
+                        color:
+                            generateColor().colorLightOrDark(pokemon.color[0])
+                                ? Colors.black
+                                : Colors.white,
+                        fontSize: 24,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: PokemonType(
+                        typeArray: pokemon.type,
+                        typeScroll: Axis.horizontal,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 20),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      pokemon.image.toString(),
+                      alignment: Alignment.center,
+                      width: 70,
+                      height: 70,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/home_pokemon_screen/home_pokemon_screen.dart
+++ lib/screens/home_pokemon_screen/home_pokemon_screen.dart
@@ -4,10 +4,12 @@ import 'package:pokedex/components/hamburger_menu.dart';
 import 'package:pokedex/components/page_default.dart';
 import 'package:pokedex/components/pokemon_type.dart';
 import 'package:pokedex/helpers/fisrtLetterCapitalizes.dart';
+import 'package:pokedex/screens/home_pokemon_screen/components/pokemon_card.dart';
 import 'package:pokedex/screens/models/pokemons.dart';
 import 'package:pokedex/services/pokemon_service.dart';
 import 'package:pokedex/themes/theme_color.dart';
 
+import '../../components/loading_screen.dart';
 import '../../helpers/generateColors.dart';
 
 class HomePokemonScreen extends StatefulWidget {
@@ -42,90 +44,18 @@ class _HomePokemonScreenState extends State<HomePokemonScreen> {
             colors: [ThemeColors.backgroudColor1, ThemeColors.backgroudColor2],
           ),
         ),
-        child: ListView.builder(
-          itemCount: database.length,
-          itemBuilder: (context, i) {
-            final Pokemons pokemon = database[i] as Pokemons;
+        child: database.isEmpty
+            ? const LoadingScreen(
+                color: Colors.black,
+              )
+            : ListView.builder(
+                itemCount: database.length,
+                itemBuilder: (context, i) {
+                  final Pokemons pokemon = database[i] as Pokemons;
 
-            return Padding(
-              padding:
-                  const EdgeInsets.only(top: 6, bottom: 6, right: 12, left: 12),
-              child: InkWell(
-                onTap: () {
-                  Navigator.pushNamed(
-                    context,
-                    "details",
-                    arguments: pokemon,
-                  );
+                  return PokemonCards(pokemon: pokemon);
                 },
-                child: Container(
-                  height: 100,
-                  width: 100,
-                  decoration: BoxDecoration(
-                    color: pokemon.color.isNotEmpty
-                        ? pokemon.color[0]
-                        : const Color(0xFF404040),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 20, top: 5),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "#${pokemon.id}",
-                              style: TextStyle(
-                                color: generateColor()
-                                        .colorLightOrDark(pokemon.color[0])
-                                    ? Colors.black54
-                                    : Colors.white54,
-                              ),
-                            ),
-                            Text(
-                              fisrtLetterCapitalizes(pokemon.name),
-                              style: TextStyle(
-                                color: generateColor()
-                                        .colorLightOrDark(pokemon.color[0])
-                                    ? Colors.black
-                                    : Colors.white,
-                                fontSize: 24,
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ),
-                            Expanded(
-                              flex: 1,
-                              child: PokemonType(
-                                typeArray: pokemon.type,
-                                typeScroll: Axis.horizontal,
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(right: 20),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Image.asset(
-                              pokemon.image.toString(),
-                              alignment: Alignment.center,
-                              width: 70,
-                              height: 70,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
               ),
-            );
-          },
-        ),
       ),
     );
   }
